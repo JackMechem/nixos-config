df5473dbf77f84645a68fb8f078c90e2842ed97e
Jack Mechem <mechemjack@gmail.com>
2026-02-26 16:45:07 -0800

    Map caps lock to escape in t480 hyprland config

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -49,6 +49,7 @@
         # --- Input ---
         input = {
           kb_layout = "us";
+          kb_options = "caps:escape";
           follow_mouse = 1;
           mouse_refocus = false;
           touchpad = {
