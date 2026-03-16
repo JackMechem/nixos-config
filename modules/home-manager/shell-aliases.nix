867755c7ff97c1293435944588f32a2cc39b5385
Jack Mechem <mechemjack@gmail.com>
2026-03-16 12:19:55 -0700

    Add nixrebs alias to rebuild dellserv config

--- modules/home-manager/shell-aliases.nix
+++ modules/home-manager/shell-aliases.nix
@@ -9,6 +9,7 @@
   home.shellAliases = {
     nixrebt = "sudo nixos-rebuild switch --flake /home/jack/nixos/#t480";
     nixrebd = "sudo nixos-rebuild switch --flake /home/jack/nixos/#desktop";
+    nixrebs = "sudo nixos-rebuild switch --flake /home/jack/nixos/#dellserv";
     v = "nvim";
     c = "clear";
     cl = "clear && ls";
