622f877f85d43d9d8ea60a894386b29c98d50e00
Jack Mechem <mechemjack@gmail.com>
2026-04-03 16:02:36 -0700

    Merge conflicts

--- hosts/dellserv/home.nix
+++ hosts/dellserv/home.nix
@@ -1,40 +1,44 @@
 {
-  config,
-  pkgs,
-  inputs,
-  ...
+    config,
+    pkgs,
+    inputs,
+    ...
 }:
 
 {
 
-  imports = [
-    ../../modules/home-manager/zsh.nix
-    ../../modules/home-manager/tmux.nix
-    ../../modules/home-manager/shell-aliases.nix
-    ../../modules/home-manager/neovimpackages.nix
-  ];
+    imports = [
+        ../../modules/home-manager/zsh.nix
+        ../../modules/home-manager/tmux.nix
+        ../../modules/home-manager/shell-aliases.nix
+        ../../modules/home-manager/neovimpackages.nix
+    ];
 
-  programs.home-manager.enable = true;
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "jack";
-  home.homeDirectory = "/home/jack";
+    programs.home-manager.enable = true;
+    # Home Manager needs a bit of information about you and the paths it should
+    # manage.
+    home.username = "jack";
+    home.homeDirectory = "/home/jack";
 
-  home.stateVersion = "25.05"; # Please read the comment before changing.
+    home.stateVersion = "25.05"; # Please read the comment before changing.
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = { };
+    # Home Manager is pretty good at managing dotfiles. The primary way to manage
+    # plain files is through 'home.file'.
+    home.file = { };
 
-  nixpkgs = {
-    config = {
-      allowUnfree = true;
-      allowUnfreePredicate = (_: true);
+    nixpkgs = {
+        config = {
+            allowUnfree = true;
+            allowUnfreePredicate = (_: true);
+        };
     };
-  };
 
-  home.sessionVariables = {
-    EDITOR = "nvim";
-  };
+    home.packages = with pkgs; [
+        claude-code
+    ];
+
+    home.sessionVariables = {
+        EDITOR = "nvim";
+    };
 
 }
